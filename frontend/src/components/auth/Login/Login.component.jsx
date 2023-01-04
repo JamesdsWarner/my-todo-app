539c3e9e4effd5c3a3fb5cc4a0a33a3cc23ed483
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-04 15:05:56 +0000

    converted into login/logout project. Added heroku backend

---
+++ frontend/src/components/auth/Login/Login.component.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import * as Styled from '../Auth.styles';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const login = async (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    try {
+      await axios.post('/api/auth/login', {
+        email,
+        password,
+      });
+      navigate('/');
+      toast.success('Login Success');
+    } catch (err) {
+      console.log(err);
+      toast.error('Login Failed');
+    }
+  };
+  return (
+    <Styled.FormWrapper>
+      <Styled.FormTitle>Login</Styled.FormTitle>
+      <Styled.Form onSubmit={login}>
+        <Styled.FormLabel htmlFor="email">
+          Email
+          <Styled.FormInput type="email" name="email" placeholder="email" required />
+        </Styled.FormLabel>
+
+        <Styled.FormLabel htmlFor="password">
+          Password
+          <Styled.FormInput type="password" name="password" placeholder="password" required />
+        </Styled.FormLabel>
+        <Styled.Button type="submit">Login</Styled.Button>
+      </Styled.Form>
+    </Styled.FormWrapper>
+  );
+};
+
+export default Login;
